fd5e0502719c420cc6070bcf5bbb655c4fe85662
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-06-07 10:42:39 -0300

    feat: remove bookmarks

--- components/CompanionCard.tsx
+++ components/CompanionCard.tsx
@@ -1,8 +1,7 @@
 "use client";
-import {addBookmark, removeBookmark} from "@/lib/actions/companion.actions";
+
 import Image from "next/image";
 import Link from "next/link";
-import {usePathname} from "next/navigation";
 
 interface CompanionCardProps {
     id: string;
@@ -21,30 +20,12 @@ const CompanionCard = ({
                            subject,
                            duration,
                            color,
-                           bookmarked,
                        }: CompanionCardProps) => {
-    const pathname = usePathname();
-    const handleBookmark = async () => {
-        if (bookmarked) {
-            await removeBookmark(id, pathname);
-        } else {
-            await addBookmark(id, pathname);
-        }
-    };
+
     return (
         <article className="companion-card" style={{backgroundColor: color}}>
             <div className="flex justify-between items-center">
                 <div className="subject-badge">{subject}</div>
-                <button className="companion-bookmark" onClick={handleBookmark}>
-                    <Image
-                        src={
-                            bookmarked ? "/icons/bookmark-filled.svg" : "/icons/bookmark.svg"
-                        }
-                        alt="bookmark"
-                        width={12.5}
-                        height={15}
-                    />
-                </button>
             </div>
 
             <h2 className="text-2xl font-bold">{name}</h2>
